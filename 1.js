e22ba5993d3f3607d66c011e7acbc6765e3f4dff
Дмитрий Баранов <baranov@semograph.com>
2018-03-27 14:39:53 +0500

    Реализована смена хода.

--- 1.js
+++ 1.js
@@ -1,52 +1,57 @@
-var chess = new Chess();
-
-var types = {
-    r: '♜',
-    n: '♞',
-    b: '♝',
-    q: '♛',
-    k: '♚',
-    p: '♟'
-};
-
 $(function () {
-    var figures = chess.board();
-    var moves = chess.moves({ verbose: true }).map(m => m.from);
-    var moves2 = chess.moves({ verbose: true }).map(m => m.from+m.to);
+    var chess = new Chess();
 
-    var table = $("<table>");
-    for(var i = 0; i < 8; i++) {
-        var row = $("<tr>");
-        for(var j = 0; j < 8; j++) {
-            let p = "abcdefgh"[j] + (8 - i);
-            var cell = $(`<td class='cell' data-id='${p}'>`);
-            row.append(cell);
-            if(figures[i][j]) {
-                var fig = $(`<div class='figure' data-id='${p}'>`);
-                fig.text(types[figures[i][j].type]);
-                fig.addClass(figures[i][j].color == "b" ? "black" : "white");
-                fig.addClass(moves.includes(p) ? "can_move" : "cant_move");
-                cell.html(fig);
+    var types = {
+        r: '♜',
+        n: '♞',
+        b: '♝',
+        q: '♛',
+        k: '♚',
+        p: '♟'
+    };
+
+    const render = function() {
+        const figures = chess.board();
+        const who_can_move = chess.moves({verbose: true}).map(m => m.from);
+        const possible_moves = chess.moves({verbose: true}).map(m => m.from + m.to);
+
+        var table = $("<table>");
+        for (var i = 0; i < 8; i++) {
+            var row = $("<tr>");
+            for (var j = 0; j < 8; j++) {
+                let p = "abcdefgh"[j] + (8 - i);
+                var cell = $(`<td class='cell' data-id='${p}'>`);
+                row.append(cell);
+                if (figures[i][j]) {
+                    var fig = $(`<div class='figure' data-id='${p}'>`);
+                    fig.text(types[figures[i][j].type]);
+                    fig.addClass(figures[i][j].color == "b" ? "black" : "white");
+                    fig.addClass(who_can_move.includes(p) ? "can_move" : "cant_move");
+                    cell.html(fig);
+                }
             }
+            table.append(row);
         }
-        table.append(row);
-    }
-    $("body").append(table);
+        $("body").html(table);
 
-    $(".figure").draggable({
-        cancel: '.cant_move',
-        containment: table,
-        revert: function (drop) {
-            return !moves2.includes($(this).data("id")+$(drop).data("id"));
-        }
-    });
-
-    $(".cell").droppable({
-        accept: ".figure",
-        drop: function(event, ui) {
-            if(moves2.includes($(ui.draggable[0]).data("id")+$(this).data("id"))) {
-                chess.move({from: $(ui.draggable[0]).data("id"), to: $(this).data("id")})
+        $(".figure").draggable({
+            cancel: '.cant_move',
+            containment: table,
+            revert: function (drop) {
+                return !possible_moves.includes($(this).data("id") + $(drop).data("id"));
             }
-        }
-    });
+        });
+
+        $(".cell").droppable({
+            accept: ".figure",
+            drop: function (event, ui) {
+                if (possible_moves.includes($(ui.draggable[0]).data("id") + $(this).data("id"))) {
+                    chess.move({from: $(ui.draggable[0]).data("id"), to: $(this).data("id")})
+                    render();
+                }
+            }
+        });
+    };
+
+    render();
 });
