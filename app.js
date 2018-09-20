230302e97022630b3276083f0494f201b33c84dd
Дмитрий Баранов <baranov@semograph.com>
2018-09-20 15:08:09 +0500

    Переводит session store на mongo

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const expressValidator = require('express-validator');
 const nunjucks = require('nunjucks');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -10,7 +12,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(expressValidator());
 app.use(express.static('static'));
-app.use(session({secret: 'keyboard cat'}));
+app.use(session({
+    secret: 'keyboard cat',
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
 
 nunjucks.configure('views', {
     autoescape: true,
