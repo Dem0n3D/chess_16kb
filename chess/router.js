d0bc146ee4845cc0bbfd15a0befc6eabca72c7d4
Дмитрий Баранов <baranov@semograph.com>
2018-09-29 11:33:56 +0500

    Реализует присоединение к партии другого игрока

--- chess/router.js
+++ chess/router.js
@@ -16,5 +16,18 @@ router.post('/game', async function (req, res) {
     res.json(game);
 });
 
+router.post('/game/join/:id', async function (req, res) {
+    const game = await Game.findOne({_id: req.params.id});
+    if (game.player2) {
+        res.status(400, "Game is already full!").end();
+    }
+    if (game.player1._id.equals(req.session.user_id)) {
+        res.status(400, "Can't play vs yourself!").end();
+    }
+    game.player2 = req.session.user_id;
+    await game.save();
+    res.json(game);
+});
+
 
 module.exports = router;
